66a5b9da026b3c8e97f8a50c66463958ded9ea9e
Clifford Vaughn <cjvaughn@rocketmail.com>
2019-05-11 19:30:16 -0700

    Updates

---
+++ precache-manifest.c7029fe9a5d21106ab5c98029bb9bfd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f24444b72f61bb1c9a34a2bef5f686",
+    "url": "/Mothers-Day/index.html"
+  },
+  {
+    "revision": "38f3ab07e671d0f728c6",
+    "url": "/Mothers-Day/static/css/main.ca6bfc83.chunk.css"
+  },
+  {
+    "revision": "3600bbdf26a268a2b447",
+    "url": "/Mothers-Day/static/js/2.d7af8b05.chunk.js"
+  },
+  {
+    "revision": "38f3ab07e671d0f728c6",
+    "url": "/Mothers-Day/static/js/main.c4cc35c1.chunk.js"
+  },
+  {
+    "revision": "77fa408bc45aeb208adb",
+    "url": "/Mothers-Day/static/js/runtime~main.553a9df5.js"
+  }
+]);
